feffa92b22a71387afdee0fcfba3c42295a4e6d6
Michel Perrocheau <michel@scalizer.fr>
2024-01-07 17:05:38 +0100

    [IMP] upgrade to v17

--- openai_connector/__manifest__.py
+++ openai_connector/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     'name': 'OpenAI Connector',
-    'version': '16.1.0.0',
+    'version': '17.0.0.0',
     'author': 'Myrrkel',
     'website': 'https://github.com/myrrkel',
     'summary': "Connector for OpenAI API",
--- openai_product_description/__manifest__.py
+++ openai_product_description/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/algpl.html).
 {
     'name': 'OpenAI Product Description',
-    'version': '16.0.0.0',
+    'version': '17.0.0.0',
     'author': 'Myrrkel',
     'website': 'https://github.com/myrrkel',
     'summary': "Generate a product sales description with OpenAI",
